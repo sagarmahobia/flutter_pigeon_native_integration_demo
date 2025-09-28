04ecc6689ee5808ca5b97245e997a9e1a2885053
Sagar Mahobia <sagarmahobia5@gmail.com>
2025-09-29 01:47:32 +0530

    add timer functionality in ios to track elapsed time and communicate with Flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,9 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private var tickerTimer: Timer?
+  private var elapsed: Int64 = 0
+
   override func application(
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -11,6 +14,25 @@ import UIKit
 
     let controller = window?.rootViewController as! FlutterViewController
     NativeCalculatorImpl().setUp(binaryMessenger: controller.binaryMessenger)
+    startTicker(binaryMessenger: controller.binaryMessenger)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func startTicker(binaryMessenger: FlutterBinaryMessenger) {
+    tickerTimer?.invalidate()
+    elapsed = 0
+    let timerEvents = TimerEvents(binaryMessenger: binaryMessenger)
+    tickerTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+      guard let self = self else { return }
+      self.elapsed += 1
+      timerEvents.onTimeElapsed(time: self.elapsed) { _ in /* ignore completion */ }
+    }
+    RunLoop.main.add(tickerTimer!, forMode: .common)
+  }
+
+  override func applicationWillTerminate(_ application: UIApplication) {
+    tickerTimer?.invalidate()
+    tickerTimer = nil
+    super.applicationWillTerminate(application)
   }
 }
